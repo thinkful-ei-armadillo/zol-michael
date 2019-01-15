82b780b6c082e3e2ef458ed5561d1ee5bf410c03
Zoljargal Fallows <zol.fallows@hotmail.com>
2019-01-15 17:18:29 +0000

    Added fn filter drill

--- hofDrills.js
+++ hofDrills.js
@@ -1,16 +1,46 @@
-function repeat(fn, n){
-    for(let i = 0; i<n; i++){
-        fn();
+// function repeat(fn, n){
+//     for(let i = 0; i<n; i++){
+//         fn();
+//     }
+// }
+
+// function hello(){
+//         console.log('Hello world');
+// }
+
+// function goodbye(){
+//     console.log('Goodbye world');
+// }
+
+// repeat(hello, 5);
+// repeat(goodbye, 5);
+
+
+// Filter drill
+
+function filter(arr, fn) {
+    // TASK: Define your function here
+    let newArr = [];
+    
+    for (let i=0; i<arr.length; i++){
+        if(fn(arr[i])) {
+            newArr.push(arr[i]);
+        }
     }
+    return newArr;
 }
 
-function hello(){
-        console.log('Hello world');
-}
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
-function goodbye(){
-    console.log('Goodbye world');
-}
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+    // This is known as a "predicate function" - it's a function that 
+    // only returns a boolean
+    return name[0] === 'R';
+});
 
-repeat(hello, 5);
-repeat(goodbye, 5);
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
